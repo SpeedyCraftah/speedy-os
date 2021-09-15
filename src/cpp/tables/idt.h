84c766d25391f3ab2bf09948ce91405d6fe01f09
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2021-09-15 12:08:38 +0100

    Update idt.h

--- src/cpp/tables/idt.h
+++ src/cpp/tables/idt.h
@@ -1,3 +1,17 @@
 #pragma once
 
-#include "stdint.h"
+#include <stdint.h>
+
+struct IDTDescriptor {
+    uint16_t Limit;
+    uint32_t Base;
+} __attribute__((packed));
+
+struct IDTEntry {
+    unsigned Offset0 : 16;
+    unsigned Selector : 16;
+    unsigned Reserved : 8;
+    unsigned Attributes : 8;
+    unsigned Offset1 : 16;
+} __attribute__((packed)) 
+__attribute__((aligned(0x10));
