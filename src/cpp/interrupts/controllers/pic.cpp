12c7f46ce5dd143f31cae6be09c40beb28c77b18
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2021-09-18 19:43:47 +0100

    Improve PIC manager

--- src/cpp/interrupts/controllers/pic.cpp
+++ src/cpp/interrupts/controllers/pic.cpp
@@ -27,17 +27,27 @@ void controllers::pic::remap(uint8_t master_offset, uint8_t slave_offset) {
     uint8_t a1 = io_port::bit_8::in(PIC1_DATA);
 	uint8_t a2 = io_port::bit_8::in(PIC2_DATA);
  
+    // Add lots of delays to let possible slow PICs catch up.
+
     // Starts init sequence for both controllers.
 	io_port::bit_8::out(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_port::bit_8::delay_void();
 	io_port::bit_8::out(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_port::bit_8::delay_void();
 
 	io_port::bit_8::out(PIC1_DATA, master_offset);
+    io_port::bit_8::delay_void();
 	io_port::bit_8::out(PIC2_DATA, slave_offset);
+    io_port::bit_8::delay_void();
 	io_port::bit_8::out(PIC1_DATA, 4);
+    io_port::bit_8::delay_void();
 	io_port::bit_8::out(PIC2_DATA, 2);
+    io_port::bit_8::delay_void();
  
 	io_port::bit_8::out(PIC1_DATA, ICW4_8086);
+    io_port::bit_8::delay_void();
 	io_port::bit_8::out(PIC2_DATA, ICW4_8086);
+    io_port::bit_8::delay_void();
  
     // Restore masks.
 	io_port::bit_8::out(PIC1_DATA, a1);
