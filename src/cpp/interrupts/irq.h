9ffae9419c69a9db9b250a4265c9c2a563f1a79e
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2021-09-21 11:59:38 +0100

    Update irq.h

--- src/cpp/interrupts/irq.h
+++ src/cpp/interrupts/irq.h
@@ -6,6 +6,9 @@
 namespace interrupts {
     class irq {
         public:
+            static uint32_t IRQ0_HANDLER;
+            static uint32_t IRQ1_HANDLER;
+        
             static void load_all();
     };
 }
