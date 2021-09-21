0c19d4a92aa8a71a376d9cb570ca587e6d42db85
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2021-09-21 11:57:26 +0100

    Update irq.cpp

--- src/cpp/interrupts/irq.cpp
+++ src/cpp/interrupts/irq.cpp
@@ -26,14 +26,7 @@ extern "C" void INTERRUPT_46();
 extern "C" void INTERRUPT_47();
 extern "C" void INTERRUPT_48();
 
-// Add events for use by mostly drivers.
-// TODO - Optimise the crap out of it.
-structures::event_handler<uint8_t, uint8_t> interrupts::irq::events;
-
 void interrupts::irq::load_all() {
-    // Load event handler.
-    events = structures::event_handler<uint8_t, uint8_t>();
-
     IDTEntries[32] = idt_define_gate(INTERRUPT_33, 0x8E);
     IDTEntries[33] = idt_define_gate(INTERRUPT_34, 0x8E);
     IDTEntries[34] = idt_define_gate(INTERRUPT_35, 0x8E);
@@ -73,7 +66,4 @@ extern "C" __attribute__((fastcall)) void HandleIRQInterrupt(uint8_t vector) {
         } 
 
     }
-
-    // Emit.
-    //interrupts::irq::events.emit_event(irq, irq);
 }
