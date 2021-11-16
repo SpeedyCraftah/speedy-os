6b735a537d1107515240d723cf6219cc675192b0
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2021-11-16 18:44:41 +0000

    Adjust example to use process ID string lookup

--- src/cpp/programs/test.cpp
+++ src/cpp/programs/test.cpp
@@ -8,7 +8,7 @@
 namespace TestProgramCPP {
     void __attribute__((fastcall)) on_keyboard_data(uint32_t id, uint32_t data) {
         if (data == '/') {
-            video::printf("\n\n[TestKeyboard] Sending end request to kernel. Bye bye!", VGA_COLOUR::LIGHT_CYAN);
+            video::printf("\n\n[TestKeyboard] Sending end request to kernel. Bye!", VGA_COLOUR::LIGHT_CYAN);
             speedyos::end_process();
         }
 
@@ -22,7 +22,9 @@ namespace TestProgramCPP {
 
         video::printf("\n[TestKeyboard] Hooking into keyboard driver...\n", VGA_COLOUR::LIGHT_CYAN);
 
-        speedyos::register_event_for_process(1, 1, on_keyboard_data);
+        uint32_t keyboard_driver_id = speedyos::fetch_process_id_by_string("Keyboard Driver");
+
+        speedyos::register_event_for_process(keyboard_driver_id, 1, on_keyboard_data);
 
         speedyos::suspend_process(1200, speedyos::SuspensionType::FULL);
         
@@ -34,14 +36,5 @@ namespace TestProgramCPP {
         video::printf("[TestKeyboard] Switching into event-only mode...\n\n", VGA_COLOUR::LIGHT_CYAN);
 
         speedyos::update_status(speedyos::TaskStatus::RUNNING_WAITING_FOR_DATA);
-
-        video::printf("hi");
-
-        while (true) {
-            speedyos::suspend_process(1000, speedyos::SuspensionType::EVENTS_ONLY);
-            video::printf("hello world\n");
-        }
-
-        speedyos::end_process();
     }
 };
