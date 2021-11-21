9d91b516d3834a7cc42ced1845b3d887a34c89d5
SpeedyCraftah <45142584+SpeedyCraftah@users.noreply.github.com>
2021-11-21 15:40:35 +0000

    Rewrite allocator

--- src/cpp/heap/allocator.cpp
+++ src/cpp/heap/allocator.cpp
@@ -1,34 +1,127 @@
 #include "allocator.h"
+#include "../scheduling/scheduler.h"
 #include <stddef.h>
 
 // Overload heap allocation operators.
 void* operator new(size_t size) {
-    return heap::malloc<void>(size);
+    return heap::malloc(size);
 }
 
 void* operator new[](size_t size) {
-    return heap::malloc<void>(size);
+    return heap::malloc(size);
 }
 
 void operator delete(void* ptr, size_t size) {
-    heap::free<void>(ptr);
+    heap::free(ptr);
 };
 
 uint32_t heap::get_allocated_bytes() {
-    return heap_allocated_bytes;
+    return total_reserved_bytes;
 }
 
 uint32_t heap::get_total_blocks() {
-    return MARRecordCounter;
+    return total_blocks;
 }
 
 uint32_t heap::get_reserved_blocks() {
     return reserved_blocks;
 }
 
-uint32_t heap::MARRecordCounter = 0;
-uint32_t heap::heap_allocated_bytes = 0;
-uint32_t heap::reserved_blocks = 0;
+void* heap::malloc(uint32_t size, bool reset, bool skip_reuse, uint32_t process_id) {
+    // For temporary use.
+    if (process_id == 0) process_id = scheduler::current_process;
+    
+    // If blocks exist and allocation allows block reuse.
+    if (total_blocks != 0 && !skip_reuse) {
+        for (uint32_t i = 0; i <= total_blocks; i++) {
+            record meta = data_meta[i];
+            
+            // If block is already reserved.
+            if (meta.reserved) continue;
 
-MARRecord* heap::mar_array = (MARRecord*)0x01000000;
-uint8_t* heap::next_alloc_addr = (uint8_t*)0x1200000;
+            // Safety check.
+            if (meta.location == 0) continue;
+
+            // If size is too small or too large.
+            if (size > meta.size || meta.size - size > 30) continue;
+
+            // Override block.
+            data_meta[i].process_id = process_id;
+            data_meta[i].reserved = true;
+
+            // If contents should be reset.
+            for (uint32_t i = 0; i < meta.size; i++) {
+                meta.location[i] = 0;
+            }
+
+            // Update statistics.
+            total_reserved_bytes += meta.size;
+            reserved_blocks += 1;
+
+            return meta.location;
+        }
+    }
+
+    // If no satisfactory existing block was found.
+
+    // Create new block.
+    uint32_t block_id = ++total_blocks;
+
+    data_meta[block_id].location = next_data_location;
+    data_meta[block_id].size = size;
+    data_meta[block_id].process_id = process_id;
+    data_meta[block_id].reserved = true;
+
+    // Update statistics.
+    total_reserved_bytes += size;
+    reserved_blocks += 1;
+
+    // Set next location.
+    next_data_location += size + 1;
+
+    return data_meta[block_id].location;
+}
+
+bool heap::free(void* ptr) {
+    for (uint32_t i = 0; i <= total_blocks; i++) {
+        record meta = data_meta[i];
+
+        // If block isn't reserved.
+        if (!meta.reserved) continue;
+        
+        // If address does not match block.
+        if (meta.location != ptr) continue;
+
+        // Free block.
+        data_meta[i].reserved = false;
+        data_meta[i].process_id = 0;
+
+        // Update statistics.
+        total_reserved_bytes -= meta.size;
+        reserved_blocks -= 1;
+
+        return true;
+    }
+
+    return false;
+}
+
+void heap::free_by_process_id(uint32_t process_id) {
+    for (uint32_t i = 0; i <= total_blocks; i++) {
+        record meta = data_meta[i];
+
+        // If block isn't reserved.
+        if (!meta.reserved) continue;
+        
+        // If address is not owned by process.
+        if (meta.process_id != process_id) continue;
+
+        // Free block.
+        data_meta[i].reserved = false;
+        data_meta[i].process_id = 0;
+
+        // Update statistics.
+        total_reserved_bytes -= meta.size;
+        reserved_blocks -= 1;
+    }
+}
--- src/cpp/heap/allocator.h
+++ src/cpp/heap/allocator.h
@@ -1,145 +1,33 @@
 #pragma once
 
-#include <stdint.h>
+#include "stdint.h"
 
-struct MARRecord {
-    // 0 = kernel.
-    uint32_t process_id = 0;
+namespace heap {
+    struct record {
+        uint32_t process_id;
+        uint32_t size;
+        uint8_t* location;
+        bool reserved;
+    } __attribute__((packed));
 
-    uint8_t* location;
-    int size;
-    bool reserved;
-};
+    // Define location of meta data array.
+    static record* data_meta = (record*)0x01000000;
 
-extern "C" uint32_t current_process;
+    // Statistics.
+    static uint32_t reserved_blocks = 0;
+    static uint32_t total_blocks = 0;
+    static uint32_t total_reserved_bytes = 0;
 
-class heap {
-    private:
-        static uint32_t heap_allocated_bytes;
+    // Location of heap data.
+    static uint8_t* next_data_location = (uint8_t*)0x1200000;
 
-        static MARRecord* mar_array;
-        static uint32_t MARRecordCounter;
-        static uint32_t reserved_blocks;
+    // Functions.
+    void* malloc(uint32_t size, bool reset = true, bool skip_reuse = false, uint32_t process_id = 0);
+    bool free(void* ptr);
 
-        static uint8_t* next_alloc_addr;
+    void free_by_process_id(uint32_t process_id);
 
-        template<typename T>
-        inline static uint8_t* to_reg_ptr(T* pointer) {
-            return (uint8_t*)(pointer);
-        }
-    
-    public:
-        // TODO: Check if MAR is within bounds.
-
-        static uint32_t get_allocated_bytes();
-        static uint32_t get_total_blocks();
-        static uint32_t get_reserved_blocks();
-
-        // Allocates {size} of memory in the heap with an option to clear old data.
-        // skip_mar should never be used.
-        template<typename T>
-        static T* malloc(uint32_t size = sizeof(T), bool reset = true, bool skip_mar = false, uint32_t process_id = current_process) {
-            T* location = nullptr;
-
-            // Iterate over the MAR to find any previous suitable locations.
-            if (!skip_mar && MARRecordCounter != 0) {
-                for (uint32_t i = 0; i < MARRecordCounter; i++) {
-                    MARRecord* block = &mar_array[i];
-
-                    // If we've reached the end of the list (left for security).
-                    if (block->location == 0) break;
-
-                    // Continue if the block is in use.
-                    if (block->reserved) continue;
-
-                    // If the block size is equal or no larger than 50 bytes of the requested size.
-                    if (block->size == size || (block->size >= size && block->size - size <= 50)) {
-                        // Clear the block off of old memory if requested.
-                        if (reset) {
-                            for (int j = 0; j <= block->size; j++) {
-                                block->location[j] = 0;
-                            }
-                        }
-
-                        block->reserved = true;
-                        block->process_id = process_id;
-                        location = (T*)block->location;
-
-                        heap_allocated_bytes += block->size;
-                        reserved_blocks++;
-                    }
-                }
-            }
-
-            // If we haven't found a satisfactory block or skip_mar has been requested.
-            if (location == nullptr) {
-                uint8_t* meta_location = next_alloc_addr;
-                
-                int mar_index = MARRecordCounter++;
-
-                uint8_t* data_location = meta_location;
-
-                // Add to MAR.
-                MARRecord record;
-                record.size = size;
-                record.location = data_location;
-                record.reserved = true;
-                record.process_id = process_id;
-                
-                mar_array[mar_index] = record;
-
-                next_alloc_addr = data_location + size;
-
-                location = (T*)data_location;
-
-                heap_allocated_bytes += size;
-                reserved_blocks++;
-            }
-
-            // TODO: Return nullptr if new block cannot be allocated.
-
-            return location;
-        };
-
-        static void free_by_process_id(uint32_t process_id) {
-            for (uint32_t i = 0; i < MARRecordCounter; i++) {
-                MARRecord* block = &mar_array[i];
-
-                // If we've reached the end of the list (left for security).
-                if (block->location == 0) break;
-
-                // Continue if the block is not reserved.
-                if (!block->reserved) continue;
-
-                // If the block is not owned by the process.
-                if (block->process_id != process_id) continue;
-
-                // Found a block, deallocate.
-                free(block->location);
-            }
-        }
-
-        // Deallocates a memory block. 1 = Deallocated, 0 = Could not deallocate.
-        template <typename T>
-        static bool free(T* address) {
-            for (uint32_t i = 0; i < MARRecordCounter; i++) {
-                MARRecord record = mar_array[i];
-                
-                // Check if record location matches freeing location.
-                if (record.location != (uint8_t*)address) continue;
-
-                // If block is not reserved, stop.
-                if (!record.reserved) break;
-
-                // Block is now free and may be reused.
-                record.reserved = false;
-
-                heap_allocated_bytes -= record.size;
-                reserved_blocks--;
-
-                return true;
-            }
-
-            return false;
-        }
-};
+    uint32_t get_allocated_bytes();
+    uint32_t get_total_blocks();
+    uint32_t get_reserved_blocks();
+}
