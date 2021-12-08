4862348be7ceed2f8a89d991785000d778db1bce
SpeedyCraftah <45142584+SpeedyCraftah@users.noreply.github.com>
2021-12-08 20:18:00 +0000

    Remove entropy from keyboard

--- src/cpp/drivers/keyboard/keyboard.cpp
+++ src/cpp/drivers/keyboard/keyboard.cpp
@@ -34,9 +34,6 @@ namespace drivers {
     // Inlined for performance.
     void keyboard::handle_interrupt() {
         uint8_t key_raw = io_port::bit_8::in(0x60);
-
-        // Add entropy for random generator.
-        random::add_entropy((key_raw * scheduler::elapsed_ms) % 100000000);
         
         // Key presses.
         char char_press = keyboard::keycode_to_ascii(key_raw, true);
