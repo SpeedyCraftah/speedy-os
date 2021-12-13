ed34a5e2101de1bcbc11ed17a03287dbad65c50a
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2021-12-13 10:38:26 +0000

    Update math.h

--- src/cpp/misc/math.h
+++ src/cpp/misc/math.h
@@ -3,8 +3,19 @@
 #include "stdint.h"
 
 namespace math {
+   // Returns the hypotenuse of the opposite and adjacent.
    float hypot(float a, float b);
+   
+   // Returns the square root of x.
    float sqrt(float x);
+   
+   // Returns the sin of x.
    float sin(float x);
+   
+   // Returns the cos of x.
    float cos(float x);
+   
+   // Returns value of PI (non-cached).
+   float PI();
+   
 }
