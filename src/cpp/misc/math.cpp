bd111633b394bb0d19d55b678131af8834d7d271
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2021-12-13 10:22:33 +0000

    Update math.cpp

--- src/cpp/misc/math.cpp
+++ src/cpp/misc/math.cpp
@@ -18,4 +18,13 @@ namespace math {
     
     return sqrt(as + bs);
   }
+  
+  float sin(float x) {
+    asm volatile("flds %0" : : "m"(x));
+    asm volatile("fsin");
+    asm volatile("sub $4, %esp");
+    asm volatile("fstps %esp");
+    asm volatile("pop %eax");
+    asm volatile("ret $4");
+  }
 }
