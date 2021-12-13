b87d97fd4e60da32e2b362ae12aca154f66d3eba
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2021-12-13 10:16:13 +0000

    Update math.cpp

--- src/cpp/misc/math.cpp
+++ src/cpp/misc/math.cpp
@@ -3,7 +3,12 @@
 #include "stdint.h"
 
 namespace math {
-  float sqrt(float x) {
-     asm volatile("
+  float __attribute__((naked)) sqrt(float x) {
+    asm volatile("flds %0" : : "m"(x));
+    asm volatile("fsqrt");
+    asm volatile("sub $4, %esp");
+    asm volatile("fstps %esp");
+    asm volatile("pop %eax");
+    asm volatile("ret $4");
   }
 }
