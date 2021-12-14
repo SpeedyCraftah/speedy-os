ef7f778fc46cc0cedf6902391d208bbf5b029b83
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2021-12-14 09:46:10 +0000

    Update graphics.h

--- src/cpp/io/graphics.h
+++ src/cpp/io/graphics.h
@@ -10,6 +10,12 @@ namespace graphics {
   extern VGA_COLOUR outline_colour = 1;
   extern uint32_t outline_width = 2;
   
+  // Plots a single pixel at the x and y coordinates.
+  void draw_pixel(uint32_t x, uint32_t y, VGA_COLOUR colour);
+  
+  // Plots a single pixel via a 1D linear coordinate.
+  void draw_pixel_linear(uint32_t x, VGA_COLOUR colour);
+  
   // Draws a rectangle. Outline width will be taken into account in the future.
   // X - Top left corner coordinate. Y - Top left corner Y coordinate. Length - Length in pixels from corner points.
   void draw_rectangle(uint32_t x, uint32_t y, uint32_t width_length, uint32_t height_length, bool fill = false);
