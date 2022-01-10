b6bc51988d8fc9e6f5b84849dbbb46cf48590f7c
SpeedyCraftah <45142584+SpeedyCraftah@users.noreply.github.com>
2022-01-10 20:14:13 +0000

    Add graphics support and GRUB structure

--- src/boot/loader.s
+++ src/boot/loader.s
@@ -10,22 +10,36 @@
 // START OF GRUB MULTIBOOT
 
 .set MAGIC, 0x1badb002
-.set FLAGS, (1<<0 | 1<<1)
+
+// Align OS to 4KB boundaries | Include memory map (TBU) | Set graphics mode.
+.set FLAGS, (1<<0 | 1<<1 | 1<<2)
+
 .set CHECKSUM, -(MAGIC + FLAGS)
 
 .section .multiboot
     .long MAGIC
     .long FLAGS
     .long CHECKSUM
+    .long 0
+    .long 0
+    .long 0
+    .long 0
+    .long 0
+    .long 0
+    .long 800
+    .long 600
+    .long 32
 
 // END OF GRUB MULTIBOOT
 
 .section .text
 .extern kernelMain
 .extern callConstructors
+.extern structure_address
 .global loader
 
 loader:
+    mov %ebx, (structure_address)
     mov $kernel_stack, %esp
     call callConstructors
     
