b6895a482ddb61b24cde926703a0cc90e7033df6
SpeedyCraftah <45142584+SpeedyCraftah@users.noreply.github.com>
2022-01-10 20:19:00 +0000

    Make random function software

--- src/cpp/misc/random.cpp
+++ src/cpp/misc/random.cpp
@@ -3,11 +3,15 @@
 static uint32_t entropy_value = 2312255697;
 
 namespace random {
-    void add_entropy(uint32_t value) {
-            value = (value * 32853) % 2187;
+    void set_seed(uint32_t seed) {
+        entropy_value = (seed * 32853) % 2187;
+    }
 
-            entropy_value += value;
-        }
+    void add_entropy(uint32_t value) {
+        value = (value * 32853) % 2187;
+
+        entropy_value += value;
+    }
 
     uint32_t next() {
         uint32_t result = entropy_value;
--- src/cpp/misc/random.h
+++ src/cpp/misc/random.h
@@ -4,5 +4,6 @@
 
 namespace random {
     uint32_t next();
+    void set_seed(uint32_t seed);
     void add_entropy(uint32_t value);
 }
