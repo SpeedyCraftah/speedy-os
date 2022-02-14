1b00669cee3a455ad948241f150a41a3980f9477
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2022-02-14 10:30:58 +0000

    Update random.h

--- src/cpp/misc/random.h
+++ src/cpp/misc/random.h
@@ -3,7 +3,11 @@
 #include "stdint.h"
 
 namespace random {
-    uint32_t next();
-    void set_seed(uint32_t seed);
-    void add_entropy(uint32_t value);
+    public:    
+        uint32_t next();
+        void set_seed(uint32_t seed);
+        void add_entropy(uint32_t value);
+    
+    private:
+        uint32_t entropy_value = 2312255697;
 }
