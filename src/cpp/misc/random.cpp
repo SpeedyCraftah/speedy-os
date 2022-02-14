692cdc4b8e34a4a30f6086bf486b810565298ea6
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2022-02-14 12:33:51 +0000

    Revert pseudo random generator changes

--- src/cpp/misc/random.cpp
+++ src/cpp/misc/random.cpp
@@ -7,13 +7,13 @@ void random::set_seed(uint32_t seed) {
 void random::add_entropy(uint32_t value) {
     value = (value * 32853) % 2187;
 
-    entropy_value *= value;
+    entropy_value += value;
 }
 
 uint32_t random::next() {
     uint32_t result = entropy_value;
 
-    add_entropy((result * 2196) * (entropy_value << (entropy_value % 10)));
+    add_entropy(result * 2196);
 
     return result;
 }
