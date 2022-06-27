f35c3ad8011627746b525193724124e503b82cb0
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2022-06-27 22:34:50 +0100

    Optimize scheduler and add debug methods and attrs

--- src/cpp/scheduling/scheduler.cpp
+++ src/cpp/scheduling/scheduler.cpp
@@ -10,23 +10,42 @@
 #include "structures/process.h"
 #include "structures/thread.h"
 #include "events.h"
+#include "../misc/memory.h"
 
 // remove after
 #include "../software/system/speedyshell/main.h"
 #include "../misc/conversions.h"
 #include "../software/include/sys.h"
+#include "../chips/pit.h"
+#include "../misc/assert.h"
 
-extern "C" Registers temporary_registers = Registers();
+// Temporary registers switched to pointer for performance.
+extern "C" Registers placeholder_registers = Registers();
+extern "C" Registers* temporary_registers = nullptr;
+extern "C" void* temporary_eip = nullptr;
 
 // Import the assembly functions.
 extern "C" void scheduler_sleep();
 extern "C" void scheduler_execute();
+extern "C" void save_fpu_boot_state();
 
+// If defined the scheduler debug runtime tools will be compiled and enabled.
+// Also enables asserts in areas where there is performance impact of doing so.
+#define SCHEDULER_DEBUG
 
 namespace scheduler {
     uint32_t elapsed_ms = 0;
+    uint32_t time_slice_ms = 5;
+    uint32_t tsc_tick_rate = 0;
+    float scheduler_idle_time_ms = 0;
+    float last_scheduler_idle_time = 0;
+    float scheduler_running_time_ms = 0;
+    float last_scheduler_running_time_ms = 0;
+    bool debug_mode = false;
+    char fpu_boot_state[108];
 
     Thread* current_thread = nullptr;
+    bool timer_preempt = false;
 
     uint32_t next_thread_id = 1;
     uint32_t next_process_id = 1;
@@ -44,12 +63,24 @@ namespace scheduler {
     Process* scheduler_event_process;
 
     void initialise() {
+        // Set base FPU state.
+        save_fpu_boot_state();
+
+        // Set default register destination.
+        temporary_registers = &placeholder_registers;
+
         // Create the structures.
         thread_list = new structures::map<Thread*>(20);
         process_list = new structures::map<Process*>(10);
         thread_event_queue = new structures::linked_array<ThreadEvent>(15);
         thread_execution_queue = new structures::linked_array<Thread*>(15);
         process_name_list = new structures::map<Process*>(10);
+
+        assert_eq("sch.structs.threads", thread_list->get_capacity(), (uint32_t)20);
+        assert_eq("sch.structs.processes", process_list->get_capacity(), (uint32_t)10);
+        assert_eq("sch.structs.events", thread_event_queue->get_capacity(), (uint32_t)15);
+        assert_eq("sch.structs.execution", thread_execution_queue->get_capacity(), (uint32_t)15);
+        assert_eq("sch.structs.procnames", process_name_list->get_capacity(), (uint32_t)10);
 
         // Create the iterators.
         thread_event_iterator = thread_event_queue->create_iterator();
@@ -60,19 +91,38 @@ namespace scheduler {
         flags.virtual_process = true;
 
         scheduler_event_process = create_process("Scheduler Events", 0, flags);
+
+        assert_eq("sch.procs.1.name.exists", process_name_list->exists("Scheduler Events"), true);
+        assert_eq("sch.procs.1.id", process_name_list->fetch("Scheduler Events")->id, scheduler_event_process->id);
+        assert_eq("sch.procs.1.id.exists", process_list->exists(scheduler_event_process->id), true);
     }
 
     extern "C" void handle_context_switch() {
         // If a thread was previously running.
         if (current_thread != nullptr) {
-            // Save the registers.
-            current_thread->registers = temporary_registers;
+            float cpu_time_used =
+                timer_preempt ? time_slice_ms : (time_slice_ms - chips::pit::fetch_channel_0_remaining_countdown());
+            
+            scheduler::scheduler_running_time_ms += cpu_time_used;
+            current_thread->process->total_cpu_time += cpu_time_used;
+
+            #ifdef SCHEDULER_DEBUG
+                uint32_t old_exec_length = thread_execution_queue->get_size();
+            #endif
 
             // Reschedule the thread.
             thread_execution_queue->push(current_thread);
 
+            #ifdef SCHEDULER_DEBUG
+                assert_eq("sch.structs.execution.length", old_exec_length + 1, thread_execution_queue->get_size());
+                assert_eq("sch.structs.execution.peek.id", thread_execution_queue->peek()->id, current_thread->id);
+            #endif
+
             // Clear current thread.
             current_thread = nullptr;
+
+            // Set temporary registers back to placeholder.
+            temporary_registers = &placeholder_registers;
         }
 
         // Reset iterator.
@@ -81,6 +131,11 @@ namespace scheduler {
         // Check the event queue for outstanding events (to be improved).
         while (thread_event_iterator.hasNext()) {
             ThreadEvent event = thread_event_iterator.next();
+
+            #ifdef SCHEDULER_DEBUG
+                assert_eq("sch.events.thread.heap", heap::allocated(event.thread), true);
+                uint32_t old_ev_length = thread_event_queue->get_size();
+            #endif
 
             // If the thread is already running an event, continue.
             if (event.thread->state.processing_event) continue;
@@ -115,6 +170,10 @@ namespace scheduler {
 
             // Remove the event from the queue.
             thread_event_iterator.remove();
+
+            #ifdef SCHEDULER_DEBUG
+                assert_eq("sch.structs.events.length", old_ev_length - 1, thread_event_queue->get_size());
+            #endif
         }
 
         // Check if any thread needs executing.
@@ -165,19 +224,30 @@ namespace scheduler {
             current_thread = thread;
 
             // Load threads registers to temporary storage.
-            temporary_registers = thread->registers;
+            temporary_registers = &thread->registers;
+            temporary_eip = reinterpret_cast<void*>(thread->registers.eip);
 
             // Switch to assembly side of scheduler to begin execution.
             return scheduler_execute();
         }
 
-        // If there is nothing available to execute, sleep.
+        // If there is nothing available to execute, add sleep time and sleep.
+        
+        scheduler_idle_time_ms += chips::pit::fetch_channel_0_remaining_countdown();
+
+        // Sleep.
         return scheduler_sleep();
     }
 
+    // Holds the data for timeslices.
+    uint32_t timer_ms_timeslice = 0;
+
     extern "C" void handle_timer_tick() {
+        // Set timer preempt state.
+        timer_preempt = true;
+
         // Increment the elapsed time.
-        elapsed_ms += 2;
+        elapsed_ms += time_slice_ms;
 
         // Handle context switch side of the timer.
         return handle_context_switch();
@@ -191,6 +261,9 @@ namespace scheduler {
         process->flags = flags;
         process->threads = new structures::linked_array<Thread*>(8);
         process->hooked_threads = new structures::linked_array<ThreadEventListener>(6);
+
+        assert_eq("sch.procs.name.heap", heap::allocated(process->name), true);
+        assert_eq("sch.procs.heap", heap::allocated(process->name), true);
 
         // Add the process to the map.
         process_list->set(process->id, process);
@@ -206,6 +279,9 @@ namespace scheduler {
             thread->flags.main_thread = true;
             thread->registers.eip = reinterpret_cast<uint32_t>(entry);
             
+            // Load the setup FPU state.
+            memcpy(thread->registers.fpu_state, fpu_boot_state, sizeof(fpu_boot_state));
+
             // Create an 8KB stack.
             thread->stack = heap::malloc(8192);
 
@@ -229,11 +305,17 @@ namespace scheduler {
         return process;
     }
 
-    Thread* create_thread(Process* process, void (*entry)()) {
+    Thread* create_thread(Process* process, void (*entry)(), void* capture) {
         Thread* thread = new Thread;
         thread->id = next_thread_id++;
         thread->process = process;
         thread->registers.eip = reinterpret_cast<uint32_t>(entry);
+
+        assert_eq("sch.procs.threads.new.heap", heap::allocated(thread), true);
+        assert_eq("sch.procs.threads.new.proc.heap", heap::allocated(process), true);
+
+        // Load the setup FPU state.
+        memcpy(thread->registers.fpu_state, fpu_boot_state, sizeof(fpu_boot_state));
         
         // Create an 8KB stack.
         thread->stack = heap::malloc(8192);
@@ -241,31 +323,38 @@ namespace scheduler {
         // Point to top of stack.
         thread->registers.esp = reinterpret_cast<uint32_t>(thread->stack) + 8192;
 
+        // Place the capture on the stack.
+        thread->registers.esp -= sizeof(uint32_t) * 2;
+        uint32_t* stack = reinterpret_cast<uint32_t*>(thread->registers.esp);
+        stack[1] = reinterpret_cast<uint32_t>(capture);
+        
         // Add the thread to the map.
         thread_list->set(thread->id, thread);
 
         // Add thread to process.
         process->threads->push(thread);
 
+        assert_eq("sch.procs.threads.list.new", thread_list->exists(thread->id), true);
+        assert_eq("sch.procs.threads.new.id", process->threads->peek()->id, thread->id);
+
         // Schedule the thread.
         thread_execution_queue->push(thread);
+
+        assert_eq("sch.procs.threads.exec.new.id", thread_execution_queue->peek()->id, thread->id);
 
         return thread;
     }
 
-    void kill_thread(Thread* thread, uint32_t code) {
-        // If the thread is the main thread for the process, kill the process.
-        if (thread->flags.main_thread) {
-            return kill_process(thread->process, code);
-        }
-
+    void kill_thread(Thread* thread, uint32_t code, bool remove_from_process) {
         // Iterate through thread process list and remove.
         auto thread_process_iterator = thread->process->threads->create_iterator();
 
-        while (thread_process_iterator.hasNext()) {
-            Thread* p_thread = thread_process_iterator.next();
+        if (remove_from_process) {
+            while (thread_process_iterator.hasNext()) {
+                Thread* p_thread = thread_process_iterator.next();
 
-            if (p_thread == thread) thread_process_iterator.remove();
+                if (p_thread == thread) thread_process_iterator.remove();
+            }
         }
 
         // Reset iterator.
@@ -293,46 +382,26 @@ namespace scheduler {
         // Deallocate and remove.
         heap::free(thread->stack);
         delete thread;
+
+        assert_eq("sch.procs.threads.delete.heap", heap::allocated(thread), false);
     }
 
     void kill_process(Process* process, uint32_t code) {
+        assert_eq("sch.procs.list.exists.1", process_list->exists(process->id), true);
+
         // Remove the process from map.
         process_list->remove(process->id);
         process_name_list->remove(process->name);
 
-        // Reset iterator.
-        thread_event_iterator.reset();
-
-        // Iterate through threads in event queue and remove.
-        while (thread_event_iterator.hasNext()) {
-            ThreadEvent event = thread_event_iterator.next();
-
-            // If process owns the thread, remove.
-            if (event.thread->process == process) thread_event_iterator.remove();
-        }
-
-        // Reset iterator.
-        thread_execution_iterator.reset();
-
-        // Iterate through execution queue and remove.
-        while (thread_execution_iterator.hasNext()) {
-            Thread* thread = thread_execution_iterator.next();
-
-            // If process owns the thread, remove.
-            if (thread->process == process) thread_execution_iterator.remove();
-        }
+        assert_eq("sch.procs.list.exists.2", process_list->exists(process->id), false);
 
         // Iterate through the processes threads and terminate.
         auto process_threads_iterator = process->threads->create_iterator();
 
         while (process_threads_iterator.hasNext()) {
             Thread* thread = process_threads_iterator.next();
-
-            thread_list->remove(thread->id);
-
-            // Deallocate objects.
-            heap::free(thread->stack);
-            heap::free(thread);
+            
+            kill_thread(thread, 1, false);
         }
 
         // Emit process end event.
@@ -348,16 +417,18 @@ namespace scheduler {
 
         // Remove the process itself.
         delete process;
+
+        assert_eq("sch.procs.delete.heap", heap::allocated(process), false);
     }
 
     void manual_context_switch_return() {
-        // Save the registers.
-        current_thread->registers = temporary_registers;
-
         // Schedule the thread.
         thread_execution_queue->push(current_thread);
 
         // Clear current thread.
         current_thread = nullptr;
+
+        // Restore temporary registers.
+        temporary_registers = &placeholder_registers;
     }
 }
--- src/cpp/scheduling/scheduler.h
+++ src/cpp/scheduling/scheduler.h
@@ -11,8 +11,19 @@
 
 namespace scheduler {
     extern uint32_t elapsed_ms;
-
+    extern uint32_t time_slice_ms;
+    extern float scheduler_idle_time_ms;
+    extern float last_scheduler_idle_time;
+    extern float scheduler_running_time_ms;
+    extern float last_scheduler_running_time_ms;
+    extern bool debug_mode;
+    extern uint32_t tsc_tick_rate;
+    extern "C" char fpu_boot_state[108];
+    
+    extern "C" bool timer_preempt;
     extern "C" Thread* current_thread;
+    extern "C" Registers* temporary_registers;
+    extern "C" Registers placeholder_registers;
 
     extern structures::map<Thread*>* thread_list;
     extern structures::map<Process*>* process_list;
@@ -26,10 +37,10 @@ namespace scheduler {
     extern "C" void handle_context_switch();
 
     Process* create_process(char* name, void (*entry)(), ProcessFlags flags = ProcessFlags());
-    Thread* create_thread(Process* process, void (*entry)());
+    Thread* create_thread(Process* process, void (*entry)(), void* capture = 0);
 
     void kill_process(Process* process, uint32_t code = 1);
-    void kill_thread(Thread* thread, uint32_t code = 1);
+    void kill_thread(Thread* thread, uint32_t code = 1, bool remove_from_process = true);
 
     // Utility function.
     void manual_context_switch_return();
