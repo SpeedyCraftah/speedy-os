00df1cd3e865e7733c6e90b13dcc443f6c4a4e09
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2024-01-08 21:43:23 +0000

    add constructor and destructor support to user programs

--- software/include/_init.cpp
+++ software/include/_init.cpp
@@ -1,21 +1,22 @@
 #include "alloc.h"
 #include "sys.h"
 
+extern "C" void (*_call_ctors)();
+extern "C" void (*_call_dtors)();
+
 extern "C" int main();
 extern "C" void _main() {
-
     // Initialise the malloc heap.
     heap::init();
 
     // Call the constructors.
-    //_init();
-    /*constructor_t* constructor;
-    for (constructor = __init_array_start; constructor < __init_array_end; constructor++) {
-        (*constructor)();
-    }*/
+    _call_ctors();
 
     // Hand over control to program main.
     int code = main();
+
+    // Call the destructors.
+    _call_dtors();
 
     // End the process.
     return speedyos::end_process(code);
