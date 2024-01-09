0fd59b05c808fe8a981df90413c5d178e78c1f32
SpeedyCraftah <45142584+SpeedyCraftah@users.noreply.github.com>
2024-01-09 16:03:41 +0000

    call constructors only if they exist

--- software/include/_init.cpp
+++ software/include/_init.cpp
@@ -9,14 +9,14 @@ extern "C" void _main() {
     // Initialise the malloc heap.
     heap::init();
 
-    // Call the constructors.
-    _call_ctors();
+    // Call the constructors (if they exist).
+    if (_call_ctors != _call_dtors) _call_ctors();
 
     // Hand over control to program main.
     int code = main();
 
-    // Call the destructors.
-    _call_dtors();
+    // Call the destructors (if they exist).
+    if (_call_ctors != _call_dtors) _call_dtors();
 
     // End the process.
     return speedyos::end_process(code);
