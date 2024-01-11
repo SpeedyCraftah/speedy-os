1375e843e3560e2e03922c1d5eef166032358d05
SpeedyCraftah <45142584+SpeedyCraftah@users.noreply.github.com>
2024-01-11 15:41:30 +0000

    make changes to assembly

--- kernel/src/asm/scheduling/scheduler.asm
+++ kernel/src/asm/scheduling/scheduler.asm
@@ -27,36 +27,10 @@ scheduler_execute:
   ; Send EOI.
   send_eoi
 
+  ; Jump to program
+  return_to_thread_ring3
+
   ; Load registers from temporary storage.
   load_general_registers_from_temp
 
-  ; Enable interrupts.
-  ; sti
-
-  ; preserve eax
-  ; todo - check for stack leak
-  push eax
-
-  mov ax, (4 * 8) | 3
-  mov ds, ax
-  mov es, ax
-  mov fs, ax
-  mov gs, ax
-
-  mov eax, esp
-  push (4 * 8) | 3 ;ds
-  push eax ;esp
-  pushfd ;eflags
-  push (3 * 8) | 3 ;cs
-  
-  mov eax, [temporary_eip]
-  push eax ;eip
-
-  ; restore eax
-  mov eax, [esp+20]
-
   iret
-  
-
-  ; Jump to programs EIP.
-  ;jmp [temporary_eip]
