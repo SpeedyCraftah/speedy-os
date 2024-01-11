1eee41dcfe6c50f24d69f8d378ded1739f1daa38
SpeedyCraftah <45142584+SpeedyCraftah@users.noreply.github.com>
2024-01-11 11:18:16 +0000

    allow ring 3 to raise the syscall interrupt

--- kernel/src/cpp/interrupts/software.cpp
+++ kernel/src/cpp/interrupts/software.cpp
@@ -2,9 +2,10 @@
 
 #include <stdint.h>
 #include "../tables/idt.h"
+#include "interrupts.h"
 
 extern "C" void INTERRUPT_128();
 
 void interrupts::software::load_all() {
-  IDTEntries[128] = idt_define_gate(INTERRUPT_128, 0x8E);
+  IDTEntries[128] = idt_define_gate(INTERRUPT_128, INT_Present | INT_GateType::INTERRUPT | INT_IOPL::RING_3);
 }
