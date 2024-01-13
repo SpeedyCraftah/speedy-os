0ba9cd83e42d362300c9877b8f7618d4130fe2fd
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2024-01-13 20:53:38 +0000

    change timer assembly

--- kernel/src/asm/scheduling/timer_tick.asm
+++ kernel/src/asm/scheduling/timer_tick.asm
@@ -10,23 +10,43 @@ global INTERRUPT_33
 INTERRUPT_33:
     ; Future optimisation idea: Check if a thread was executing or not before saving.
 
-    ; Save registers to structure with stack offset to counter stack difference.
-    save_general_registers_to_temp 12
+    ; Save all registers (+stack).
+    push eax
+    mov eax, [virtual_temporary_registers]
 
-    ; Save returning program EIP to temporary register.
-    mov eax, [esp]
-    mov [ecx+32], eax
+    mov [eax+4], ecx
+    mov [eax+8], edx
+    mov [eax+12], ebx
+    mov [eax+20], ebp
+    mov [eax+24], esi
+    mov [eax+28], edi
+    fsave [eax+40]
 
-    ; Jump to higher level interrupt handler.
-    mov [esp], dword .far_return
+    ;esp
+    mov ecx, [esp+4+12]
+    mov [eax+16], ecx
+
+    ;eip
+    mov ecx, [esp+4+0]
+    mov [eax+32], ecx
+
+    ;eflags
+    mov ecx, [esp+4+8]
+    mov [eax+36], ecx
+
+    ;eax
+    pop ecx
+    mov [eax+0], ecx
+
+
+    ; Set kernel stack.
+    mov eax, [kernel_stack]
+    mov [esp+12], eax
+
+    ; Disable interrupts and set to IOPL=0 in eflags.
+    mov [esp+8], dword 00000000001000000000000000000010b
+
+    ; Load scheduler address.
+    mov [esp+0], handle_timer_tick
+
     iret
-
-    .far_return:
-        ; Disable interrupts.
-        cli
-
-        ; Load the kernel stack.
-        load_kernel_stack
-
-        ; Jump to C++ code.
-        jmp handle_timer_tick
