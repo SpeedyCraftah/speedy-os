232b66a433bf3b067961acd6865706d92b0035ae
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2024-01-14 12:06:09 +0000

    add conditional register save on timer tick

--- kernel/src/asm/scheduling/timer_tick.asm
+++ kernel/src/asm/scheduling/timer_tick.asm
@@ -3,50 +3,65 @@
 extern virtual_temporary_registers
 extern handle_timer_tick
 extern temporary_eip
+extern current_thread
 
 global INTERRUPT_33
 
 ; On PIT timer yield (context switch).
 INTERRUPT_33:
-    ; Future optimisation idea: Check if a thread was executing or not before saving.
-
-    ; Save all registers (+stack).
     push eax
-    mov eax, [virtual_temporary_registers]
+    mov eax, [current_thread]
+    test eax, eax
+    je .scheduler_sleep_finish
 
-    mov [eax+4], ecx
-    mov [eax+8], edx
-    mov [eax+12], ebx
-    mov [eax+20], ebp
-    mov [eax+24], esi
-    mov [eax+28], edi
-    fsave [eax+40]
+    .thread_exec_finish:
+        ; Save all registers (+stack).
+        mov eax, [virtual_temporary_registers]
 
-    ;esp
-    mov ecx, [esp+4+12]
-    mov [eax+16], ecx
+        mov [eax+4], ecx
+        mov [eax+8], edx
+        mov [eax+12], ebx
+        mov [eax+20], ebp
+        mov [eax+24], esi
+        mov [eax+28], edi
+        fsave [eax+40]
 
-    ;eip
-    mov ecx, [esp+4+0]
-    mov [eax+32], ecx
+        ;esp
+        mov ecx, [esp+4+12]
+        mov [eax+16], ecx
 
-    ;eflags
-    mov ecx, [esp+4+8]
-    mov [eax+36], ecx
+        ;eip
+        mov ecx, [esp+4+0]
+        mov [eax+32], ecx
 
-    ;eax
-    pop ecx
-    mov [eax+0], ecx
+        ;eflags
+        mov ecx, [esp+4+8]
+        mov [eax+36], ecx
+
+        ;eax
+        pop ecx
+        mov [eax+0], ecx
 
 
-    ; Set kernel stack.
-    mov eax, [kernel_stack]
-    mov [esp+12], eax
+        ; Set kernel stack.
+        mov eax, [kernel_stack]
+        mov [esp+12], eax
 
-    ; Disable interrupts and set to IOPL=0 in eflags.
-    mov [esp+8], dword 00000000001000000000000000000010b
+        ; Disable interrupts and set to IOPL=0 in eflags.
+        mov [esp+8], KERNEL_EFLAGS
 
-    ; Load scheduler address.
-    mov [esp+0], handle_timer_tick
+        ; Load scheduler address.
+        mov [esp+0], dword handle_timer_tick
 
-    iret
+        iret
+
+    .scheduler_sleep_finish:
+        add esp, 4
+
+        ; Load scheduler address.
+        mov [esp+0], dword handle_timer_tick
+
+        ; Disable interrupts and set to IOPL=0 in eflags.
+        mov [esp+8], KERNEL_EFLAGS
+
+        iret
