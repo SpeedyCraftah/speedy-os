5b6c1793170f9a007ba1565996b0ba01d89b7df4
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2024-02-04 18:38:27 +0000

    change datasink push to set

--- kernel/src/cpp/scheduling/datasink.cpp
+++ kernel/src/cpp/scheduling/datasink.cpp
@@ -19,7 +19,7 @@ namespace scheduler {
 
             assert_eq("sch.structs.sdatasink.fragments", sink->fragments.get_capacity(), (uint32_t)10);
 
-            process->steady_sinks->push(sink);
+            process->steady_sinks->set(sink->handle_id, sink);
             active_sinks.set(sink->handle_id, sink);
             return sink;
         }
