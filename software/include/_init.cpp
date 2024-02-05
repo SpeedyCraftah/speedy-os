9a104859292fbd2e4a6afeed33722d63f4103bcd
SpeedyCraftah <45142584+SpeedyCraftah@users.noreply.github.com>
2024-02-05 13:05:19 +0000

    add random stack canary on runtime

--- software/include/_init.cpp
+++ software/include/_init.cpp
@@ -2,7 +2,7 @@
 #include "sys.h"
 
 // Stack protector definitions.
-uint32_t __stack_chk_guard = 0x38325789;
+uint32_t __stack_chk_guard = 0xd3adb33f;
 extern "C" void __stack_chk_fail(void) {
     speedyos::speedyshell::printf("[FATAL] Stack protector detected change to canary");
     speedyos::end_process(1);
@@ -14,6 +14,9 @@ extern "C" void (*_call_dtors)();
 
 extern "C" int main();
 extern "C" void _main() {
+    // Randomize the stack canary (current MS time for now).
+    __stack_chk_guard = speedyos::fetch_elapsed_time();
+
     // Initialise the malloc heap.
     heap::init();
 
