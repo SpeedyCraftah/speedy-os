3e81c3489bedba9acc72a24d78de458bd55bdc5b
Speedy <45142584+SpeedyCraftah@users.noreply.github.com>
2024-02-08 22:18:12 +0000

    add test function for user space graphics

--- software/src/testprogram/main.cpp
+++ software/src/testprogram/main.cpp
@@ -3,6 +3,7 @@
 #include "../../../shared/map.h"
 #include "../../../shared/flex_array.h"
 #include "../../../shared/memory.h"
+#include "../../../shared/graphics/graphics.h"
 
 structures::map<char*> cool_map;
 structures::map<char*> cool_map2;
@@ -12,11 +13,8 @@ structures::flexible_array<int> cool_flex;
 int main() {
     speedyos::speedyshell::printf("running!\n");
 
-    uint32_t* graphics = speedyos::upgrade_graphics();
-    graphics[0] = 0xFFFFFFFF;
-    graphics[1] = 0xFFFFFFFF;
-    graphics[2] = 0xFFFFFFFF;
-    graphics[3] = 0xFFFFFFFF;
+    uint32_t* graphics_ptr = speedyos::upgrade_graphics();
+    graphics::init(graphics_ptr, speedyos::fetch_graphics_resolution(), speedyos::fetch_colour_depth());
 
     return 0;
 }
