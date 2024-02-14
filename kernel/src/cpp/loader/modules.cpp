4c7dfffb1115daf3dc56df10ca0639025f13e871
SpeedyCraftah <45142584+SpeedyCraftah@users.noreply.github.com>
2024-02-14 14:49:45 +0000

    add multiboot module header and print names

--- kernel/src/cpp/loader/modules.cpp
+++ kernel/src/cpp/loader/modules.cpp
@@ -1,5 +1,8 @@
 #include "modules.h"
 
+#include "../io/video.h"
+#include "../../../../shared/conversions.h"
+
 namespace modules {
     static uint8_t* structure_address;
 
@@ -8,5 +11,16 @@ namespace modules {
 
         uint32_t mod_count = *(uint32_t*)(structure_address + 20);
         uint32_t* mod_addr_first = reinterpret_cast<uint32_t*>(*(uint32_t*)(structure_address + 24));
+
+        for (int i = 0; i < mod_count; i++) {
+            modules::MultibootModule* mod = reinterpret_cast<modules::MultibootModule*>(mod_addr_first + (i * 4));
+
+            video::printf("Module ");
+            video::printf(conversions::s_int_to_char(i + 1));
+            video::printf(" name: ");
+            video::printf(mod->string);
+            video::printnl();
+            video::printnl();
+        }
     }
 };
--- kernel/src/cpp/loader/modules.h
+++ kernel/src/cpp/loader/modules.h
@@ -5,5 +5,12 @@
 // Handles the Grub modules and converts it to a list of loadable programs.
 
 namespace modules {
+    struct MultibootModule {
+        uint8_t* mod_start;
+        uint8_t* mod_end;
+        char* string;
+        uint32_t _reserved;
+    }__attribute__((packed));
+
     void discover(uint8_t* structure_address);
 }
