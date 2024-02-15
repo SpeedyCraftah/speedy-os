31368e483e35541c92bb9414381a8ea6d4a07e8f
SpeedyCraftah <45142584+SpeedyCraftah@users.noreply.github.com>
2024-02-15 11:31:31 +0000

    add module loading and parsing

--- kernel/src/cpp/loader/modules.cpp
+++ kernel/src/cpp/loader/modules.cpp
@@ -6,6 +6,8 @@
 
 namespace modules {
     static uint8_t* structure_address;
+    uint32_t modules_count;
+    MultibootModule* modules;
 
     void discover(uint8_t* structure_addr) {
         structure_address = structure_addr;
@@ -13,22 +15,41 @@ namespace modules {
         uint32_t mod_count = *(uint32_t*)(structure_address + 20);
         uint32_t* mod_addr_first = reinterpret_cast<uint32_t*>(*(uint32_t*)(structure_address + 24));
 
+        modules_count = mod_count;
+        modules = (MultibootModule*)kmalloc(sizeof(MultibootModule) * mod_count);
+
         for (int i = 0; i < mod_count; i++) {
-            modules::MultibootModule* mod = reinterpret_cast<modules::MultibootModule*>(mod_addr_first + (i * 4));
+            MultibootModule& module = modules[i];
+            RawMultibootModule* mod = reinterpret_cast<RawMultibootModule*>(mod_addr_first + (i * 4));
             auto args = structures::string(mod->string).split_by(' ');
+
+            // NOTE - this is on the heap.
             char* mod_name = args[0];
+
+            module.name = mod_name;
+            module.type = MultibootModule::SOFTWARE_EXECUTABLE;
+            module.size = reinterpret_cast<uint32_t>(mod->mod_end) - reinterpret_cast<uint32_t>(mod->mod_start);
+            module.data = mod->mod_start;
 
             video::printf("Discovered module: ");
             video::printf(mod_name);
-            video::printnl();
 
             for (int j = 1; j < args.get_size(); j++) {
-                auto arg = args[j];
+                auto arg_raw = args[j];
+                structures::string arg = arg_raw;
+                
+                if (arg == "interface-provider") {
+                    module.type = MultibootModule::SOFTWARE_EXECUTABLE_INTERFACE_PROVIDER;
+                }
 
-
-
-                delete arg;               
+                delete arg_raw;               
             }
+
+            video::printf(", type ");
+            video::printf(conversions::u_int_to_char(module.type));
+            video::printnl();
         }
+
+        video::printnl();
     }
 };
--- kernel/src/cpp/loader/modules.h
+++ kernel/src/cpp/loader/modules.h
@@ -5,12 +5,26 @@
 // Handles the Grub modules and converts it to a list of loadable programs.
 
 namespace modules {
-    struct MultibootModule {
+    struct RawMultibootModule {
         uint8_t* mod_start;
         uint8_t* mod_end;
         char* string;
         uint32_t _reserved;
     }__attribute__((packed));
+
+    struct MultibootModule {
+        char* name;
+        enum : uint8_t {
+            SOFTWARE_EXECUTABLE,
+            SOFTWARE_EXECUTABLE_INTERFACE_PROVIDER
+        } type;
+
+        uint32_t size;
+        uint8_t* data;
+    };
+
+    extern uint32_t modules_count;
+    extern MultibootModule* modules;
 
     void discover(uint8_t* structure_address);
 }
